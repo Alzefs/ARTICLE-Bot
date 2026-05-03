1b63fb17e540b386fcbc7e4f04e84ee943c6d40c
Alzefs <emircan.de93@gmail.com>
2026-05-03 13:13:48 +0200

    Initialize Discord bot with ping command

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return;
+
+    if (message.content === "!ping") {
+        message.reply("🏓 Pong!");
+    }
+});
+
+client.once("ready", () => {
+    console.log(`Bot aktif: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
